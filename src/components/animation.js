7d333701962eec069fb257c813d1b51e6e13e969
Rida R Syed <ridarafisyed@yahoo.com>
2020-07-20 12:07:31 +0500

    build fix

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,16 +1,16 @@
 /** @format */
 
 import React from "react";
-// import { AliceGame } from "./aliceGame";
+import { AliceGame } from "./aliceGame";
 // import { RabbitHole } from "./rabbitHole";
 import { RedQueenRace } from "./redQueenRace";
 
 export default function animation() {
 	return (
 		<div>
-			{/* <AliceGame /> */}
+			<AliceGame />
 			{/* <RabbitHole /> */}
-			<RedQueenRace />
+			{/* <RedQueenRace /> */}
 		</div>
 	);
 }
